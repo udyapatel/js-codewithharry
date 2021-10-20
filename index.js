26f30fb7401b03f47d4eb3ea5f26ebe1b424081f
udyapatel <udya.patel2050@gmail.com>
2021-10-20 20:37:33 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.log('Hello console');
+console.log(45*45);
+
+
